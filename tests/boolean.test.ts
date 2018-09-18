3f8e4d5ca3791cbed1b2097763e3e201a2b8902f
Guillaume Salles <guillaume.salles@me.com>
2018-09-18 23:37:22 +0200

    add false test for boolean

--- tests/boolean.test.ts
+++ tests/boolean.test.ts
@@ -3,7 +3,11 @@ import { createCliForTest, stdout } from "./utils";
 describe("boolean", () => {
   const callCli = createCliForTest("./fixtures/boolean.ts");
 
-  test("should handle boolean", async () => {
-    expect(await callCli(["--x", "true"])).toEqual(stdout("true"));
+  test("should handle true value", async () => {
+    expect(await callCli(["--x"])).toEqual(stdout("true"));
+  });
+
+  test("should handle false value", async () => {
+    expect(await callCli([""])).toEqual(stdout("false"));
   });
 });
